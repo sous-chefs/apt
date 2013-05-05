81e5f621d1b6c0d7fdc17a54f56cd3702c833f10
jtimberman <joshua@opscode.com>
2013-05-05 17:38:59 -0600

    [COOK-2885] - Add attribute, exclude node
    * Set an attribute (caching_server) to true if the node should be a
      caching server. This is done in the cacher-ng recipe.
    * Search for the server based on the above attribute.
    * Exclude the node itself from search, else apt may fail to install
      packages.
    * Update metadata with attributes.

--- metadata.rb
+++ metadata.rb
@@ -12,3 +12,19 @@ recipe            "apt::cacher-client", "Client for the apt::cacher-ng caching p
 %w{ ubuntu debian }.each do |os|
   supports os
 end
+
+attribute "apt/cacher-client/restrict_environment",
+  :description => "Whether to restrict the search for the caching server to the same environment as this node",
+  :default => false
+
+attribute "apt/cacher_port",
+  :description => "Default listen port for the caching server",
+  :default => "3142"
+
+attribute "apt/key_proxy",
+  :description => "Passed as the proxy passed to GPG for the apt_repository resource"
+  :default => ""
+
+attribute "apt/caching_server",
+  :description => "Set this to true if the node is a caching server"
+  :default => false
