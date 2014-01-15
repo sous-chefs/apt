d89e6ec9767386a54ff8e1402de7858fda7f26b5
Kent Shultz <kentmshultz@gmail.com>
2014-01-15 17:03:39 -0600

    add Rackspace branding to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,19 @@
 name              "rackspace_apt"
-maintainer        "Opscode, Inc."
-maintainer_email  "cookbooks@opscode.com"
+maintainer        "Rackspace, US Inc."
+maintainer_email  "kent.shultz@rackspace.com"
 license           "Apache 2.0"
 description       "Configures apt and apt services and LWRPs for managing apt repositories and preferences"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "3.0.0"
 recipe            "rackspace_apt", "Runs apt-get update during compile phase and sets up preseed directories"
 recipe            "rackspace_apt::cacher-ng", "Set up an apt-cacher-ng caching proxy"
-recipe            "rackspace_apt::cacher-client", "Client for the apt::cacher-ng caching proxy"
+recipe            "rackspace_apt::cacher-client", "Client for the rackspace_apt::cacher-ng caching proxy"
 
 %w{ ubuntu debian }.each do |os|
   supports os
 end
 
-attribute "rackspace_apt/cacher_client/restrict_environment",
+attribute "rackspace_apt/switch/cacher_client/restrict_environment",
   :description => "Whether to restrict the search for the caching server to the same environment as this node",
   :default => "false"
 
