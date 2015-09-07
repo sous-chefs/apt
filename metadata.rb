48179de79af831ca703564fc434f8aecb639ff58
Tim Smith <tsmith84@gmail.com>
2015-09-07 14:32:16 -0700

    Ignore kitchen dir

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,17 @@
-name              'apt'
-maintainer        'Chef Software, Inc.'
-maintainer_email  'cookbooks@chef.io'
-license           'Apache 2.0'
-description       'Configures apt and apt services and LWRPs for managing apt repositories and preferences'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-issues_url        'https://github.com/opscode-cookbooks/apt/issues' if respond_to?(:issues_url)
-source_url        'https://github.com/opscode-cookbooks/apt' if respond_to?(:source_url)
-version           '2.8.2'
-recipe            'apt', 'Runs apt-get update during compile phase and sets up preseed directories'
-recipe            'apt::cacher-ng', 'Set up an apt-cacher-ng caching proxy'
-recipe            'apt::cacher-client', 'Client for the apt::cacher-ng caching proxy'
+name 'apt'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Configures apt and apt services and LWRPs for managing apt repositories and preferences'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+issues_url 'https://github.com/opscode-cookbooks/apt/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/opscode-cookbooks/apt' if respond_to?(:source_url)
+version '2.8.2'
+recipe 'apt', 'Runs apt-get update during compile phase and sets up preseed directories'
+recipe 'apt::cacher-ng', 'Set up an apt-cacher-ng caching proxy'
+recipe 'apt::cacher-client', 'Client for the apt::cacher-ng caching proxy'
 
-%w{ ubuntu debian }.each do |os|
+%w(ubuntu debian).each do |os|
   supports os
 end
 
