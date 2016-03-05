3bd10dd96f16b729c46885ecec059584f14ddda7
Jennifer Davis <iennae@gmail.com>
2016-03-04 22:49:57 -0800

    Adding custom matchers for apt_update new resource.

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,4 +1,9 @@
 if defined?(ChefSpec)
+
+  #################
+  # apt_preference
+  #################
+
   def add_apt_preference(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:apt_preference, :add, resource_name)
   end
@@ -7,11 +12,27 @@ if defined?(ChefSpec)
     ChefSpec::Matchers::ResourceMatcher.new(:apt_preference, :remove, resource_name)
   end
 
+  #################
+  # apt_repository
+  #################
+
   def add_apt_repository(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:apt_repository, :add, resource_name)
   end
 
   def remove_apt_repository(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:apt_repository, :remove, resource_name)
+  end
+
+  #################
+  # apt_update
+  #################
+
+  def update_apt_update(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:apt_update, :update, resource_name)
+  end
+
+  def periodic_apt_update(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:apt_update, :periodic, resource_name)
   end
 end
