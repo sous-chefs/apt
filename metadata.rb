15a414392ecb0d8cc8b124c7a241ea9d154efde2
Tim Smith <tsmith@chef.io>
2017-02-01 11:32:56 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -16,6 +16,6 @@ end
 
 source_url 'https://github.com/chef-cookbooks/apt'
 issues_url 'https://github.com/chef-cookbooks/apt/issues'
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'compat_resource', '>= 12.16.3'
