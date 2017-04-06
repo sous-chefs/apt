619e8daf3f88196921599559b3512368fd254bcf
Tim Smith <tsmith@chef.io>
2017-04-05 22:02:48 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,8 +2,8 @@
 # Cookbook:: apt
 # Recipe:: default
 #
-# Copyright:: 2008-2016, Chef Software, Inc.
-# Copyright:: 2009-2016, Bryan McLellan <btm@loftninjas.org>
+# Copyright:: 2008-2017, Chef Software, Inc.
+# Copyright:: 2009-2017, Bryan McLellan <btm@loftninjas.org>
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
