667fd5bd1f7a72890245491516ed4271d7e19446
Tim Smith <tsmith84@gmail.com>
2019-08-05 19:56:48 -0700

    Remove the recipe description from the metadata
    This is unused
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,6 @@ description 'Configures apt and apt caching.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '7.1.1'
 
-recipe 'apt::default', 'Runs apt-get update during compile phase and sets up preseed directories'
-recipe 'apt::cacher-ng', 'Set up an apt-cacher-ng caching proxy'
-recipe 'apt::cacher-client', 'Client for the apt::cacher-ng caching proxy'
-
 %w(ubuntu debian).each do |os|
   supports os
 end
